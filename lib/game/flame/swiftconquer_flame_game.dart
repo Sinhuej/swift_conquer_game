2c941a21e98a56fa34b3afa2b52114b0267a05fd
Don Francis <sinhuej@gmail.com>
2025-12-19 12:25:01 -0500

    Phase 32: stub Flame layer for green CI (no input yet)

--- lib/game/flame/swiftconquer_flame_game.dart
+++ lib/game/flame/swiftconquer_flame_game.dart
@@ -1,31 +1,8 @@
 import 'package:flame/game.dart';
-import 'package:flame/input.dart';
 
-import '../core/game_loop.dart';
-import 'render/world_renderer.dart';
-
-class SwiftConquerFlameGame extends FlameGame
-    with TapCallbacks, LongPressCallbacks {
-  final GameLoop loop = GameLoop();
-
+class SwiftConquerFlameGame extends FlameGame {
   @override
   Future<void> onLoad() async {
-    add(WorldRenderer(loop.world));
-  }
-
-  @override
-  void update(double dt) {
-    super.update(dt);
-    loop.tick(dt);
-  }
-
-  @override
-  void onTapDown(TapDownEvent event) {
-    // Phase 32: selection hook
-  }
-
-  @override
-  void onLongPressStart(LongPressStartEvent event) {
-    // Phase 32: box select / command hook
+    // Phase 32 stub: ECS + renderer run outside Flame for now
   }
 }

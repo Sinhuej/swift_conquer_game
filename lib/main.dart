f6c6c85ca3edfd478d2300d7ff4ff04c539bd33c
Your Name <your@email.com>
2026-03-28 11:33:05 -0400

    Lock demo build to landscape and immersive fullscreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
 import 'screens/game_screen.dart';
 
-void main() {
+Future<void> _applyDemoSystemUi() async {
+  await SystemChrome.setPreferredOrientations(const [
+    DeviceOrientation.landscapeLeft,
+    DeviceOrientation.landscapeRight,
+  ]);
+
+  await SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+}
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await _applyDemoSystemUi();
   runApp(const SwiftConquerApp());
 }
 
-class SwiftConquerApp extends StatelessWidget {
+class SwiftConquerApp extends StatefulWidget {
   const SwiftConquerApp({super.key});
+
+  @override
+  State<SwiftConquerApp> createState() => _SwiftConquerAppState();
+}
+
+class _SwiftConquerAppState extends State<SwiftConquerApp>
+    with WidgetsBindingObserver {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+    _applyDemoSystemUi();
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.resumed) {
+      _applyDemoSystemUi();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
